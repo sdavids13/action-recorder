8ca1f1bbe69ca333e648a50a03c2914fcd8384dd
Steve Davids <sdavids@gmail.com>
2016-06-08 20:47:09 -0400

    Let Spring Boot wire up the hal+json support

--- src/main/java/com/sdavids/ActionRecorderApplication.java
+++ src/main/java/com/sdavids/ActionRecorderApplication.java
@@ -2,11 +2,7 @@ package com.sdavids;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.hateoas.config.EnableHypermediaSupport;
 
-import static org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
-
-@EnableHypermediaSupport(type = HypermediaType.HAL)
 @SpringBootApplication
 public class ActionRecorderApplication {
 
